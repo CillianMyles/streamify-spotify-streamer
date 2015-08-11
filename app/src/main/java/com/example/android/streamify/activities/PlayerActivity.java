09cc1d851d6474f94701d9e34a9fa5b3a61a0f4e
Cillian <mylsey4thewin@gmail.com>
2015-08-11 12:20:56 +0100

    Player screen now loading details correctly. Need to work on UI behaviour.

--- app/src/main/java/com/example/android/streamify/activities/PlayerActivity.java
+++ app/src/main/java/com/example/android/streamify/activities/PlayerActivity.java
@@ -2,50 +2,96 @@ package com.example.android.streamify.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.android.streamify.R;
+import com.example.android.streamify.StreamifyApplication;
+import com.example.android.streamify.tasks.PlayerTask;
 import com.example.android.streamify.utilities.Constants;
+
+import kaaes.spotify.webapi.android.SpotifyService;
 
 public class PlayerActivity extends AppCompatActivity {
 
     private static final String TAG = PlayerActivity.class.getSimpleName();
 
+    private static final String TRACK_ID_KEY = "track_id_key";
+
     private String mTrackId;
+
+    private TextView mArtistName;
+    private TextView mAlbumName;
+    private ImageView mAlbumCover;
+    private TextView mSongName;
+    private SeekBar mPlayTime;
+    private ImageButton mPrevious;
+    private ImageButton mPlayPause;
+    private ImageButton mNext;
+
+    private SpotifyService mSpotify;
+
+    private String mPreviewUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
+        if (savedInstanceState != null && savedInstanceState.containsKey(TRACK_ID_KEY)) {
+            mTrackId = savedInstanceState.getString(TRACK_ID_KEY);
+        }
+
         getIntentExtras();
         initialiseUI();
         setUpSpotify();
-        populateTrackUI();
+        populatePlayerUI();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (mTrackId != null) {
+            outState.putString(TRACK_ID_KEY, mTrackId);
+        }
+        super.onSaveInstanceState(outState);
     }
 
     private void getIntentExtras() {
         if (getIntent().hasExtra(Constants.TRACK_ID_TAG)) {
             mTrackId = getIntent().getExtras().getString(Constants.TRACK_ID_TAG);
+            Log.e(TAG, "Track ID : " + mTrackId);
         }
     }
 
     private void initialiseUI() {
-
+        mArtistName = (TextView) findViewById(R.id.player_tv_artist_name);
+        mAlbumName = (TextView) findViewById(R.id.player_tv_album_name);
+        mSongName = (TextView) findViewById(R.id.player_tv_song_name);
+        mAlbumCover = (ImageView) findViewById(R.id.player_img_album_cover);
+        mPlayTime = (SeekBar) findViewById(R.id.player_seek_bar_play_time);
+        mPrevious = (ImageButton) findViewById(R.id.player_btn_previous);
+        mPlayPause = (ImageButton) findViewById(R.id.player_btn_play_pause);
+        mNext = (ImageButton) findViewById(R.id.player_btn_next);
     }
 
     private void setUpSpotify() {
-
+        mSpotify = StreamifyApplication.getSpotifyService();
     }
 
-    private void populateTrackUI() {
-
+    private void populatePlayerUI() {
+        PlayerTask task = new PlayerTask(mSpotify, mArtistName, mAlbumName, mAlbumCover, mSongName,
+                mPlayTime, mPrevious, mPlayPause, mNext);
+        task.execute(mTrackId);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_player, menu);
         return true;
     }
--- app/src/main/java/com/example/android/streamify/tasks/PlayerTask.java
+++ app/src/main/java/com/example/android/streamify/tasks/PlayerTask.java
@@ -1,11 +1,19 @@
 package com.example.android.streamify.tasks;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.example.android.streamify.StreamifyApplication;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Track;
@@ -16,50 +24,94 @@ import retrofit.client.Response;
 /**
  * Represents an asynchronous task used to get info on a particular track.
  */
-public class PlayerTask extends AsyncTask<String, Void, Track> {
+public class PlayerTask extends AsyncTask<String, Void, String> {
 
     private final String TAG = TracksTask.class.getSimpleName();
 
     private Track mTrack;
+    private String mPreviewUrl;
+    private Boolean mPlaying;
+    private MediaPlayer mMediaPlayer;
 
     private SpotifyService mSpotify;
     private TextView mArtistName;
     private TextView mAlbumName;
     private ImageView mAlbumCover;
     private TextView mSongName;
+    private SeekBar mPlayTime;
+    private ImageButton mPrevious;
+    private ImageButton mPlayPause;
+    private ImageButton mNext;
 
-    public PlayerTask(SpotifyService spotifyService, TextView artistName,
-                      TextView albumName, ImageView albumCover, TextView songName) {
+    public PlayerTask(SpotifyService spotifyService, TextView artistName, TextView albumName,
+                      ImageView albumCover, TextView songName, SeekBar playTime,
+                      ImageButton previous, ImageButton playPause, ImageButton next) {
         this.mSpotify = spotifyService;
         this.mArtistName = artistName;
         this.mAlbumName = albumName;
         this.mAlbumCover = albumCover;
         this.mSongName = songName;
+        this.mPlayTime = playTime;
+        this.mPrevious = previous;
+        this.mPlayPause = playPause;
+        this.mNext = next;
     }
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+        mPlaying = false;
+    }
+
+    private void initialisePlayPause() {
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mMediaPlayer.setDataSource(mPreviewUrl);
+            mMediaPlayer.prepare();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not stream content", e);
+        } catch (IllegalArgumentException e) {
+            Log.e(TAG, "Incorrect arguments", e);
+        }
+        mPlayPause.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (!mPlaying) {
+                    mMediaPlayer.start();
+                    mPlaying = true;
+                } else {
+                    mMediaPlayer.pause();
+                    mPlaying = false;
+                }
+            }
+        });
     }
 
     @Override
-    protected Track doInBackground(String... params) {
+    protected String doInBackground(String... params) {
 
         if (!params[0].equals("")) {
 
             mSpotify.getTrack(params[0], new Callback<Track>() {
                 @Override
                 public void success(Track track, Response response) {
+                    mArtistName.setText(track.artists.get(0).name);
+                    mAlbumName.setText(track.album.name);
+                    Picasso.with(StreamifyApplication.getContext())
+                            .load(track.album.images.get(0).url)
+                            .into(mAlbumCover);
+                    mSongName.setText(track.name);
+                    mPreviewUrl = track.preview_url;
                     mTrack = track;
                 }
 
                 @Override
                 public void failure(RetrofitError error) {
-                    mTrack = new Track();
+
                 }
             });
 
-            return mTrack;
+            return mPreviewUrl;
 
         } else {
             return null;
@@ -67,16 +119,9 @@ public class PlayerTask extends AsyncTask<String, Void, Track> {
     }
 
     @Override
-    protected void onPostExecute(final Track track) {
-        if (track.id != null) {
-            mArtistName.setText(track.artists.get(0).name);
-            mAlbumName.setText(track.album.name);
-            if (track.album.images.size() > 0) {
-                Picasso.with(StreamifyApplication.getContext())
-                        .load(track.album.images.get(0).url)
-                        .into(mAlbumCover);
-            }
-            mSongName.setText(track.name);
+    protected void onPostExecute(final String previewUrl) {
+        if (previewUrl != null) {
+            initialisePlayPause();
         }
     }
 }
